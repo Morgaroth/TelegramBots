8a10729c579dd1d2decec221eb25977f8f4ca872
Mateusz Jaje <mateuszjaje@gmail.com>
2015-09-24 17:10:03 +0200

    implemented basic boobs bot

--- src/main/scala/io/github/morgaroth/telegram/bot/bots/CyckoBot.scala
+++ src/main/scala/io/github/morgaroth/telegram/bot/bots/CyckoBot.scala
@@ -2,7 +2,7 @@ package io.github.morgaroth.telegram.bot.bots
 
 import akka.actor.{Actor, ActorLogging, Props}
 import com.mongodb.MongoException.DuplicateKey
-import com.mongodb.casbah.Implicits._
+import com.mongodb.casbah.Imports.mongoCollAsScala
 import com.mongodb.casbah.commons.MongoDBObject
 import com.mongodb.casbah.commons.conversions.scala.RegisterJodaTimeConversionHelpers
 import com.mongodb.casbah.{Imports, MongoClient, MongoClientURI, WriteConcern}
@@ -10,13 +10,13 @@ import com.novus.salat.Context
 import com.novus.salat.annotations.Key
 import com.novus.salat.dao.SalatDAO
 import com.novus.salat.global.ctx
+import io.github.morgaroth.telegram.bot.bots.CyckoBot.PublishBoobs
 import io.github.morgaroth.telegram.bot.core.api.models._
 import io.github.morgaroth.telegram.bot.core.engine.NewUpdate
 import io.github.morgaroth.telegram.bot.core.engine.core.BotActor.{Handled, HandledUpdate}
-import org.bson.types.ObjectId
 import org.joda.time.DateTime
 
-import scala.util.Try
+import scala.util.{Random, Try}
 
 /**
  * Created by mateusz on 23.09.15.
@@ -45,10 +45,10 @@ trait BoobsListenerDao {
 
   def collection: Option[String] = None
 
-  lazy val dao = new SalatDAOWithCfg[Files](uri, collection.getOrElse("listeners")) {}
+  lazy val dao = new SalatDAOWithCfg[BoobsListener](uri, collection.getOrElse("listeners")) {}
 }
 
-case class Files(@Key("_id") fileId: String, typ: String, created: DateTime = DateTime.now())
+case class Files(@Key("_id") fileId: String, typ: String, random: Double = Random.nextDouble(), created: DateTime = DateTime.now())
 
 trait FilesDao {
   def uri: String
@@ -56,9 +56,15 @@ trait FilesDao {
   def collection: Option[String] = None
 
   lazy val dao = new SalatDAOWithCfg[Files](uri, collection.getOrElse("files")) {}
+
+  def random(x: Int) = {
+    dao.find(MongoDBObject("random" -> MongoDBObject("$gt" -> Random.nextDouble()))).sort(MongoDBObject("random" -> -1)).take(x)
+  }
 }
 
 object CyckoBot {
+
+  private[CyckoBot] case class PublishBoobs(f: Files, owner: Int)
 
   def props() =
     Props(classOf[CyckoBot])
@@ -66,7 +72,10 @@ object CyckoBot {
 
 class CyckoBot extends Actor with ActorLogging {
 
-  val dao = new FilesDao {
+  val FilesDao = new FilesDao {
+    override def uri: String = "mongodb://localhost/CyckoBot"
+  }
+  val SubsDao = new BoobsListenerDao {
     override def uri: String = "mongodb://localhost/CyckoBot"
   }
 
@@ -76,26 +85,41 @@ class CyckoBot extends Actor with ActorLogging {
       val g = m.text.get
       g.stripPrefix("/") match {
         case getImages if getImages.startsWith("get") =>
-          val numberStr = g.stripPrefix("get").takeWhile(_ != " ")
-          val count = Try(numberStr.trim.toInt).toOption.getOrElse(10)
-          dao.dao.find(MongoDBObject.empty).take(count).toList.foreach(fId =>
-            sender() ! (if (fId.typ == "document") SendDocument(m.chatId, Right(fId.fileId)) else SendPhoto(m.chatId, Right(fId.fileId)))
+          val numberStr = g.stripPrefix("get").dropWhile(_ == " ").takeWhile(_ != " ")
+          val count = Math.min(Try(numberStr.trim.toInt).toOption.getOrElse(1), 5)
+          FilesDao.random(count).foreach(fId =>
+            sender() ! SendBoobsCorrectType(m.chatId, fId)
           )
 
-        case getAll if getAll.startsWith("all") =>
-          dao.dao.find(MongoDBObject.empty).foreach(fId =>
-            sender() ! (if (fId.typ == "document") SendDocument(m.chatId, Right(fId.fileId)) else SendPhoto(m.chatId, Right(fId.fileId)))
+        case getRandom if getRandom.startsWith("random") =>
+          FilesDao.random(1).foreach(fId =>
+            sender() ! SendBoobsCorrectType(m.chatId, fId)
           )
+        case subscribe if subscribe.startsWith("subscribe") =>
+          val response = Try {
+            SubsDao.dao.insert(BoobsListener(m.chatId.toString, m.chatId))
+            "This chat is subscribed!"
+          }.recover {
+            case e: DuplicateKey => "This chat is already subscribed"
+            case anot => s"Another error ${anot.getMessage}"
+          }.get
+          sender() ! SendMessage(m.chatId, response)
+
+        case subscribe if subscribe.startsWith("unsubscribe") =>
+          val wr = SubsDao.dao.remove(MongoDBObject("chatId" -> m.chatId))
+          val response = if (wr.getN == 0) "Unsubscribed (newer was)" else "Unsubscribed :("
+          sender() ! SendMessage(m.chatId, response)
+
         case delete if delete.startsWith("delete") =>
           if (m.reply_to_message.isDefined) {
             if (m.reply_to_message.get.document.isDefined) {
               val toRemove = m.reply_to_message.get.document.get.file_id
-              val a: Imports.WriteResult = dao.dao.remove(MongoDBObject("_id" -> toRemove))
+              val a: Imports.WriteResult = FilesDao.dao.remove(MongoDBObject("_id" -> toRemove))
               sender() ! SendMessage(m.chatId, "Deleted.")
             } else if (m.reply_to_message.get.photo.isDefined) {
               val photos = m.reply_to_message.get.photo.get
               val biggest: String = photos.sortBy(_.width).last.file_id
-              val a: Imports.WriteResult = dao.dao.remove(MongoDBObject("_id" -> biggest))
+              val a: Imports.WriteResult = FilesDao.dao.remove(MongoDBObject("_id" -> biggest))
               sender() ! SendMessage(m.chatId, "Deleted.")
             } else {
               sender() ! SendMessage(m.chatId, "Sorry, this message isn't a photo/file comment of reply.")
@@ -106,21 +130,23 @@ class CyckoBot extends Actor with ActorLogging {
         case help if help.startsWith("help") =>
           val text =
             """
-              |Hello, here CyckoBot, I wil provide for You the best boobs!
+              |Hello, here CyckoBot, I will provide the *best boobs* for You!
               |
               |My boobs database is build by You, send me images, gifs and I save it for
               |You and Your camrades.
               |
               |Commands:
-              |/get N - sends You N boobs images from database
-              |/get - sends You 10 boobs images from database
-              |/all - sends You all boobs from database (not recommended ( ͡° ͜ʖ͡°) )
+              |/get N - sends You N boobs images from database, max 5
+              |/get - sends You 5 boobs images from database
+              |/random - sends You random boobs from database, enjoy it ( ͡° ͜ʖ͡°)
               |/delete - only as reply comment to another image, deletes it from database
-              |/listen - subscribes this conversation for boobs news
-              |/unlisten - unubscribes this conversation from boobs news
+              |/subscribe - subscribes this conversation for boobs news
+              |/unsubscribe - unubscribes this conversation from boobs news
               |/help - prints this message
             """.stripMargin
           sender() ! SendMessage(m.chatId, text, Some("Markdown"))
+        case illegal if m.reply_to_message.isDefined =>
+          // ignore, someone only commented Bot message
         case unknown =>
           sender() ! SendMessage(m.chatId, s"Sorry, I dont know command $unknown.")
       }
@@ -132,7 +158,9 @@ class CyckoBot extends Actor with ActorLogging {
       val photos = m.photo.get
       val biggest: PhotoSize = photos.sortBy(_.width).last
       val response = Try {
-        dao.dao.insert(Files(biggest.file_id, "photo"))
+        val files = Files(biggest.file_id, "photo")
+        FilesDao.dao.insert(files)
+        self.forward(PublishBoobs(files, m.chatId))
         "Thx!"
       }.recover {
         case e: DuplicateKey => "This image is already in DataBase"
@@ -146,7 +174,9 @@ class CyckoBot extends Actor with ActorLogging {
         val mime = doc.mime_type.get
         if (Set("image/jpeg", "image/png", "image/gif") contains mime) {
           Try {
-            dao.dao.insert(Files(doc.file_id, "document"))
+            val files = Files(doc.file_id, "document")
+            FilesDao.dao.insert(files)
+            self.forward(PublishBoobs(files, m.chatId))
             "Thx!"
           }.recover {
             case e: DuplicateKey => "This image is already in DataBase"
@@ -159,5 +189,14 @@ class CyckoBot extends Actor with ActorLogging {
         "Mime type not defined, I suck"
       }
       sender() ! HandledUpdate(id, SendMessage(m.chatId, response))
+
+    case PublishBoobs(f, owner) =>
+      SubsDao.dao.find(MongoDBObject("chatId" -> MongoDBObject("$ne" -> owner))).foreach(listener =>
+        sender() ! SendBoobsCorrectType(listener.chatId, f)
+      )
+  }
+
+  def SendBoobsCorrectType(to: Int, fId: Files): Command = {
+    if (fId.typ == "document") SendDocument(to, Right(fId.fileId)) else SendPhoto(to, Right(fId.fileId))
   }
 }
